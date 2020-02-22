c813f24889104ccbb52a38f7a197fdd6360992c3
DebarunMitra <debarun96mitra@gmail.com>
2020-02-23 00:58:50 +0530

    custom function in city

--- LB_First_App/common/models/city.js
+++ LB_First_App/common/models/city.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(City) {
+                                  /**
+                                   * populate the city
+                                   * @param {number} count number of people to populate
+                                   * @param {Function(Error, object)} callback
+                                   */
 
-};
+                                  City.prototype.pop = function(
+                                    count,
+                                    callback
+                                  ) {
+                                    if (count <= 0){
+                                      let err={
+                                        statusCode:"400",
+                                        message:"You need to populate with one or more people"
+                                      };
+                                      callback(err);
+                                    }
+                                    else{
+                                          let result = {
+                                            status: `Region populate by ${count} people`
+                                          };
+                                          // TODO
+                                          callback(null, result);
+                                        }
+                                  };
+                                };
